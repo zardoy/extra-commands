09c23002c80c749bf0486a4485513f52baf6813d
Vitaly Turovsky <vital2580@icloud.com>
2021-10-17 14:14:11 +0300

    next

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,23 @@
-import { VSCodeFramework } from 'vscode-framework'
-import vscode from 'vscode'
 import fs from 'fs'
+import { join } from 'path'
+import vscode from 'vscode'
+import { VSCodeFramework } from 'vscode-framework'
+import untildify from 'untildify'
 
 export const activate = (ctx: vscode.ExtensionContext) => {
     const framework = new VSCodeFramework(ctx)
-    framework.registerCommand('open-extension-folder', (_, extensionId: string) => {
-        const path = vscode.extensions.getExtension(extensionId)!.extensionPath
-        console.log(path)
+    framework.registerCommand('open-extension-folder', async (_, extensionId: string) => {
+        const extensionsPath =
+            process.env.NODE_ENV === 'development'
+                ? process.platform === 'win32'
+                    ? '%USERPROFILE%\\.vscode\\extensions'
+                    : '~/.vscode/extensions'
+                : join(ctx.extensionPath, '..')
+
+        const extensionsDirs = await fs.promises.readdir(untildify(extensionsPath))
+
+        // const extensions = extensionsDirs.
+
+        console.log(extensionsPath, extensionsDirs)
     })
 }
