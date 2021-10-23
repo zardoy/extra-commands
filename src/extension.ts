993a3647ffb6d89275195c6fed67c01eb3dd80a2
Vitaly Turovsky <vital2580@icloud.com>
2021-10-23 10:39:12 +0300

    add new commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,10 @@
 import fs from 'fs'
-import { join } from 'path'
+import { join, posix } from 'path'
 import vscode from 'vscode'
 import { extensionCtx, registerExtensionCommand } from 'vscode-framework'
 import untildify from 'untildify'
 import fastFolderSize from 'fast-folder-size'
+import { Utils } from 'vscode-uri'
 
 // TODO fight for releasing
 const getExtensionsDir = () =>
@@ -21,5 +22,76 @@ export const activate = () => {
     })
     registerExtensionCommand('show-extensionos-sizes', async () => {
         // const size = await new Promise<number>(resolve => fastFolderSize(getExtensionsDir()))
+    })
+    registerExtensionCommand('go-to-line', async () => {
+        const { activeTextEditor } = vscode.window
+        if (!activeTextEditor) return
+        const inputBox = vscode.window.createInputBox()
+        inputBox.title = 'Go to Line.'
+        // inputBox.placeholder = '+ / - are relative to the current line'
+        inputBox.onDidChangeValue(str => {
+            inputBox.validationMessage = ''
+            if (!str) return
+            if (!/[+-]?\d+(:\d+)?/.test(str)) {
+                inputBox.validationMessage = 'Wrong format number:number'
+                return
+            }
+
+            const line = Number.parseInt(str)
+            if (line > activeTextEditor.document.lineCount) inputBox.validationMessage = 'Maximum line exceeded'
+        })
+        inputBox.onDidAccept(() => {
+            if (inputBox.validationMessage) return
+            const lineNumber = +inputBox.value
+            const line = activeTextEditor.document.lineAt(lineNumber)
+            // TODO needs tab
+            const startPos = new vscode.Position(lineNumber - 1, line.firstNonWhitespaceCharacterIndex)
+            activeTextEditor.selection = new vscode.Selection(startPos, startPos)
+            activeTextEditor.revealRange(activeTextEditor.selection, vscode.TextEditorRevealType.InCenterIfOutsideViewport)
+            inputBox.hide()
+        })
+        inputBox.onDidHide(inputBox.dispose)
+        inputBox.show()
+    })
+    registerExtensionCommand('go-to-relative-path', async () => {
+        const currentUri = vscode.window.activeTextEditor?.document.uri
+        if (!currentUri) {
+            await vscode.window.showWarningMessage('No opened text editor')
+            return
+        }
+
+        const { fs } = vscode.workspace
+        const selectedPath = await new Promise<string>(resolve => {
+            // TODO implement multistep
+            let currentPath = '..'
+            const quickPick = vscode.window.createQuickPick<vscode.QuickPickItem & { type: vscode.FileType; name: string }>()
+            const updateItems = async () => {
+                const filesList = await fs.readDirectory(Utils.joinPath(currentUri, currentPath))
+                quickPick.items = filesList.map(([name, type]) => ({
+                    name,
+                    type,
+                    label: `$(${type === vscode.FileType.Directory ? 'file-directory' : 'file'}) ${name}`,
+                    description: name,
+                }))
+            }
+
+            // BUSY
+            void updateItems()
+            quickPick.onDidHide(quickPick.dispose)
+            quickPick.onDidAccept(() => {
+                const selectedItem = quickPick.activeItems[0]!
+                const itemPath = posix.join(currentPath, selectedItem.name)
+                if (selectedItem.type === vscode.FileType.Directory) {
+                    currentPath = itemPath
+                    void updateItems()
+                } else {
+                    quickPick.hide()
+                    resolve(itemPath)
+                }
+            })
+            quickPick.show()
+        })
+        if (selectedPath === undefined) return
+        await vscode.workspace.openTextDocument(Utils.joinPath(currentUri, selectedPath))
     })
 }
