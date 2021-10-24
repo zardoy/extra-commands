c23b8fa04e4095aaa8cd7ce23a6dbfb9c52cb9eb
Vitaly Turovsky <vital2580@icloud.com>
2021-10-24 22:13:00 +0300

    implement 2 new commands and terminal

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,11 @@
 import fs from 'fs'
 import { join, posix } from 'path'
 import vscode from 'vscode'
-import { extensionCtx, registerExtensionCommand } from 'vscode-framework'
+import { extensionCtx, registerActiveDevelopmentCommand, registerExtensionCommand, registerNoop } from 'vscode-framework'
 import untildify from 'untildify'
 import fastFolderSize from 'fast-folder-size'
 import { Utils } from 'vscode-uri'
+import ansiEscapes from 'ansi-escapes'
 
 // TODO fight for releasing
 const getExtensionsDir = () =>
@@ -14,7 +15,7 @@ const getExtensionsDir = () =>
             : '~/.vscode/extensions'
         : join(extensionCtx.extensionPath, '..')
 
-export const activate = () => {
+export const activate = async () => {
     registerExtensionCommand('open-extension-folder', async (_, extensionId: string) => {
         const extensionsDirs = await fs.promises.readdir(untildify(getExtensionsDir()))
 
@@ -47,7 +48,8 @@ export const activate = () => {
             // TODO needs tab
             const startPos = new vscode.Position(lineNumber - 1, line.firstNonWhitespaceCharacterIndex)
             activeTextEditor.selection = new vscode.Selection(startPos, startPos)
-            activeTextEditor.revealRange(activeTextEditor.selection, vscode.TextEditorRevealType.InCenterIfOutsideViewport)
+            // always in center. always in focus
+            activeTextEditor.revealRange(activeTextEditor.selection, vscode.TextEditorRevealType.InCenter)
             inputBox.hide()
         })
         inputBox.onDidHide(inputBox.dispose)
@@ -93,5 +95,69 @@ export const activate = () => {
         })
         if (selectedPath === undefined) return
         await vscode.workspace.openTextDocument(Utils.joinPath(currentUri, selectedPath))
+    })
+    registerExtensionCommand('add-import', async () => {
+        // for TS files only
+        // TODO this command will be removed from here in favor of TS plugin
+        const editor = vscode.window.activeTextEditor
+        if (editor === undefined) return
+        // const nextImportIndex = /^(?!import)/m.exec(editor.document.getText())?.index ?? 0
+        let nextImportLine = 1
+        let lineIndex = 0
+        for (const line of editor.document.getText().split('\n')) {
+            lineIndex++
+            if (line.startsWith('import')) continue
+            nextImportLine = lineIndex - 1
+            break
+        }
+
+        const currentPos = editor.selection.start
+        await editor.insertSnippet(new vscode.SnippetString("import { $2 } from '$1'\n"), new vscode.Position(nextImportLine, 0))
+        const { dispose } = vscode.window.onDidChangeTextEditorSelection(({ selections }) => {
+            const currentLine = selections[0]!.start.line
+            if (currentLine <= nextImportLine) return
+            // looses selections and mutl-selections
+            editor.selection = new vscode.Selection(currentPos.translate(1), currentPos.translate(1))
+            dispose()
+        })
+    })
+    registerExtensionCommand('open-terminal-without-focus', () => {
+        vscode.window.activeTerminal?.show(true)
+    })
+    registerActiveDevelopmentCommand(() => {
+        const writeEmitter = new vscode.EventEmitter<string>()
+        let line = ''
+        const terminal = vscode.window.createTerminal({
+            name: `My Extension REPL`,
+            pty: {
+                onDidWrite: writeEmitter.event,
+                open: () => writeEmitter.fire('Type and press enter to echo the text\r\n\r\n'),
+                close: () => {},
+                handleInput: (data: string) => {
+                    // https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797#general-ascii-codes
+                    const codes = {
+                        backspace: 127,
+                        //
+                        ctrlBackspace: 23,
+                        del: 27,
+                    }
+                    if (data.charCodeAt(0) === 127) {
+                        // backspace
+                        writeEmitter.fire(`\b${ansiEscapes.eraseEndLine}`)
+                        return
+                    }
+
+                    if (data === '\r') {
+                        writeEmitter.fire(`\r\necho: "${line}"\r\n\n`)
+                        line = ''
+                    } else {
+                        console.log('data', data.charCodeAt(0))
+                        line += data
+                        writeEmitter.fire(data)
+                    }
+                },
+            },
+        })
+        terminal.show()
     })
 }
