636e6d64352a7fbb26338746338e5bf688b59281
Vitaly Turovsky <vital2580@icloud.com>
2023-01-12 03:54:00 +0300

    fix: make text edit commands work in notebooks (they're now using standard `registerTextEditorCommand`)

--- src/textCommands/pasteReplaceLineContents.ts
+++ src/textCommands/pasteReplaceLineContents.ts
@@ -1,22 +1,17 @@
 // eslint-disable-next-line unicorn/import-style
-import { env, Range } from 'vscode'
-import { getActiveRegularEditor } from '@zardoy/vscode-utils'
-import { registerExtensionCommand } from 'vscode-framework'
+import { commands, env, Range } from 'vscode'
+import { getExtensionCommandId } from 'vscode-framework'
 
 export default () => {
     // reason: selectLineContents -> paste works, but it messes with your selection in undo stack, this one is super clean
-    registerExtensionCommand('pasteReplaceLineContents', async (_, options: { trimEnd?: boolean } = {}) => {
-        const editor = getActiveRegularEditor()
-        if (!editor) return
-        const { trimEnd = true } = options
-        let replaceContent = await env.clipboard.readText()
+    commands.registerTextEditorCommand(getExtensionCommandId('pasteReplaceLineContents'), async (editor, edit, options = {}) => {
+        const { trimEnd = true, content } = options
+        let replaceContent = content ?? (await env.clipboard.readText())
         if (trimEnd) replaceContent = replaceContent.trimEnd()
-        const { document } = editor
-        void editor.edit(edit => {
-            // eslint-disable-next-line curly
-            for (const selection of editor.selections) {
-                edit.replace(new Range(selection.start.with(undefined, 0), document.lineAt(selection.end).range.end), replaceContent.replace(/\n\r?$/, ''))
-            }
-        })
+        const { document, selections } = editor
+        // eslint-disable-next-line curly
+        for (const selection of selections) {
+            edit.replace(new Range(selection.start.with(undefined, 0), document.lineAt(selection.end).range.end), replaceContent.replace(/\n\r?$/, ''))
+        }
     })
 }
--- src/textCommands/removeSurroundingCharacter.ts
+++ src/textCommands/removeSurroundingCharacter.ts
@@ -1,19 +1,15 @@
 import * as vscode from 'vscode'
-import { registerExtensionCommand } from 'vscode-framework'
+import { getExtensionCommandId } from 'vscode-framework'
 
 export default () => {
     // use case: when surrounding braces can't be deleted with bracketeer (e.g. bad embedded support) bad easy to select
-    registerExtensionCommand('removeSurroundingCharacter', async () => {
+    vscode.commands.registerTextEditorCommand(getExtensionCommandId('removeSurroundingCharacter'), async (editor, edit) => {
         // TODO create something like also for withing selection (this works with outer sel)
-        const editor = vscode.window.activeTextEditor
-        if (!editor || editor.viewColumn === undefined) return
-        await editor.edit(edit => {
-            for (const selection of editor.selections)
-                for (const [pos, delta] of [
-                    [selection.start, -1],
-                    [selection.end, 1],
-                ] as Array<[vscode.Position, number]>)
-                    edit.delete(new vscode.Range(pos.translate(0, delta), pos))
-        })
+        for (const selection of editor.selections)
+            for (const [pos, delta] of [
+                [selection.start, -1],
+                [selection.end, 1],
+            ] as Array<[vscode.Position, number]>)
+                edit.delete(new vscode.Range(pos.translate(0, delta), pos))
     })
 }
