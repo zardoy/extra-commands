211f3de084fdef9067681f3e0e40ba8684d3a8ce
Vitaly Turovsky <vital2580@icloud.com>
2023-01-13 05:53:48 +0300

    fix: fix basic usage of pasteReplaceLineContents + do reset selection when cursor at the end of current line

--- src/textCommands/pasteReplaceLineContents.ts
+++ src/textCommands/pasteReplaceLineContents.ts
@@ -1,17 +1,25 @@
 // eslint-disable-next-line unicorn/import-style
-import { commands, env, Range } from 'vscode'
+import { commands, env, Range, Selection } from 'vscode'
 import { getExtensionCommandId } from 'vscode-framework'
 
 export default () => {
     // reason: selectLineContents -> paste works, but it messes with your selection in undo stack, this one is super clean
-    commands.registerTextEditorCommand(getExtensionCommandId('pasteReplaceLineContents'), async (editor, edit, options = {}) => {
+    commands.registerTextEditorCommand(getExtensionCommandId('pasteReplaceLineContents'), async (editor, _edit, options = {}) => {
         const { trimEnd = true, content } = options
         let replaceContent = content ?? (await env.clipboard.readText())
         if (trimEnd) replaceContent = replaceContent.trimEnd()
         const { document, selections } = editor
-        // eslint-disable-next-line curly
-        for (const selection of selections) {
-            edit.replace(new Range(selection.start.with(undefined, 0), document.lineAt(selection.end).range.end), replaceContent.replace(/\n\r?$/, ''))
+        const pos = selections[0]!.end
+        const resetSelection = selections.length === 1 && selections[0]!.start.isEqual(pos) && pos.isEqual(document.lineAt(pos).range.end)
+        await editor.edit(edit => {
+            // eslint-disable-next-line curly
+            for (const selection of selections) {
+                edit.replace(new Range(selection.start.with(undefined, 0), document.lineAt(selection.end).range.end), replaceContent)
+            }
+        })
+        if (resetSelection) {
+            const newPos = document.lineAt(pos).range.end
+            editor.selections = [new Selection(newPos, newPos)]
         }
     })
 }
