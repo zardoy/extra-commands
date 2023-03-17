31e3d0812addd4bb8558d681aa85e3514f506753
Vitaly Turovsky <vital2580@icloud.com>
2023-03-17 03:12:15 +0300

    first tested launch: now I'm using it with "settingsSync.keybindingsPerPlatform": false

--- src/platformKeybindings.ts
+++ src/platformKeybindings.ts
@@ -1,8 +1,42 @@
 import * as vscode from 'vscode'
-import { extensionCtx, registerActiveDevelopmentCommand, registerExtensionCommand } from 'vscode-framework'
-import { getNodeValue, parseTree } from 'jsonc-parser'
+import { extensionCtx, registerExtensionCommand } from 'vscode-framework'
+import { getNodeValue, parse, parseTree } from 'jsonc-parser'
+import { compact, pickObj } from '@zardoy/utils'
 
 const SCHEME = 'extraCommands.keybindings'
+
+// universal keybinding usually has two source keybinding with the same id:
+// the first one for win and second for mac
+type UniversalKeybindingId = number
+type KeybindingOutput = Partial<Record<'key' | 'command' | 'when' | 'win' | 'mac', string>> & { universal?: UniversalKeybindingId }
+
+const _initialCategories = {
+    universal: null,
+    win: null,
+    mac: null,
+    linux: null,
+    other: null,
+    removed: null,
+}
+
+type AllKeybindingsOutput = Record<keyof typeof _initialCategories, KeybindingOutput[]>
+
+const platformWhenContextMap = {
+    win: 'isWindows',
+    mac: 'isMac',
+    linux: 'isLinux',
+}
+
+const universalPlatformKey = Object.keys(platformWhenContextMap)
+
+const getCategoriesWithEmptyArrays = () => Object.fromEntries(Object.keys(_initialCategories).map(key => [key, [] as any[]]))
+
+const specialModifiers = {
+    ctrlCmd: {
+        mac: 'cmd',
+        win: 'ctrl',
+    },
+}
 
 export default () => {
     const keybindingsFile =
@@ -27,6 +61,15 @@ export default () => {
                 const keybindingsArr = {
                     $ref: '#/definitions/keybindings',
                 }
+                const universalKeybindingsArr = {
+                    $ref: '#/definitions/universalKeybindings',
+                }
+                const stringProp = {
+                    type: 'string',
+                }
+                const commandEnumToPatch = originalSchema.items.properties.command.anyOf[0]
+                // remove annoying - suggestions
+                commandEnumToPatch.enum = commandEnumToPatch.enum.filter(c => !c.startsWith('-'))
                 const schema = {
                     allowTrailingCommas: true,
                     allowComments: true,
@@ -37,10 +80,22 @@ export default () => {
                             type: 'array',
                             items: originalSchema.items,
                         },
+                        universalKeybindings: {
+                            type: 'array',
+                            items: {
+                                ...originalSchema.items,
+                                required: [],
+                                defaultSnippets: [],
+                                properties: {
+                                    ...originalSchema.items.properties,
+                                    ...Object.fromEntries(Object.keys(platformWhenContextMap).map(key => [key, stringProp])),
+                                },
+                            },
+                        },
                         ...originalSchema.definitions,
                     },
                     properties: {
-                        universal: keybindingsArr,
+                        universal: universalKeybindingsArr,
                         windows: keybindingsArr,
                         mac: keybindingsArr,
                         linux: keybindingsArr,
@@ -53,52 +108,106 @@ export default () => {
             const source = await vscode.workspace.fs.readFile(keybindingsFile).then(val => val.toString())
             const tree = parseTree(source, [], { allowEmptyContent: true, allowTrailingComma: true })
             if (tree?.type !== 'array') throw new Error('Invalid keybindings.json: Root object must be array')
-            const categories = {
-                universal: [] as any[],
-                win: [],
-                mac: [],
-                linux: [],
-                other: [] as any[],
-            }
+
+            const categories = getCategoriesWithEmptyArrays()
+
+            type KeybindingSource = Partial<Record<'key' | 'command' | 'when', string>> & { universal?: UniversalKeybindingId }
             let prevOffset = tree.offset + 1 //+1 for [
             for (const child of tree.children!) {
                 if (child.type !== 'object') continue
-                const value: Partial<Record<'key' | 'command' | 'when' | 'win' | 'mac' | 'linux', string>> & { unversal?: number } = getNodeValue(child)
+
+                const value: KeybindingSource = getNodeValue(child)
                 const prevOffsetFixed = prevOffset
                 prevOffset = child.offset + child.length
                 if (source.at(prevOffset) === ',') prevOffset++
 
                 const pushKeybinding = (key: string, obj = value) => {
-                    categories[key].push({ ...obj, /* content between */ comment: source.slice(prevOffsetFixed, child.offset) })
+                    categories[key].push({
+                        ...obj,
+                        /* content between */ comment: source
+                            .slice(prevOffsetFixed, child.offset)
+                            .trimStart()
+                            .replace(/( |\t)+$/, ''),
+                    })
                 }
 
-                if (value.unversal) {
-                    // handle num
-                    pushKeybinding('other', value)
+                if (value.command?.startsWith('-')) {
+                    pushKeybinding('removed')
                     continue
                 }
 
-                const mapWhenContext = {
-                    win: 'isWindows',
-                    mac: 'isMac',
-                    linux: 'isLinux',
+                if (value.universal !== undefined) {
+                    // handle num
+                    pushKeybinding('universal', value)
+                    continue
                 }
-                const mappedContextType = value.when && Object.entries(mapWhenContext).find(([, context]) => new RegExp(`^${context}( |$)`).exec(value.when!))
+
+                // #region push win, mac, linux keybindings
+                const mappedContextType = getPlatformFromContext(value.when)
                 if (mappedContextType) {
-                    const newObj = { ...value, when: value.when!.slice(0, mappedContextType[1].length + 3).trim() }
-                    pushKeybinding(mappedContextType[0], newObj)
+                    const newObj = { ...value, when: mappedContextType.newWhen }
+                    pushKeybinding(mappedContextType.key, newObj)
                     continue
                 }
 
                 pushKeybinding('other')
             }
+            // #endregion
 
-            categories.universal = categories.universal.map(({ universal, ...x }) => x)
-            for (const key of Object.keys(categories)) {
-                // categories[key] = categories[key].map(() => )
+            let output = '{\n'
+            // #region form universal keybindings output
+            categories.universal = categories.universal.reduce<Array<KeybindingSource & { comment? }>>((prev, current: KeybindingSource & { comment? }) => {
+                if (current.universal === undefined) return prev
+                const originalKeybinding = prev.find(({ universal }) => universal === current.universal)
+                const platformMatch = getPlatformFromContext(current.when)
+                if (platformMatch) {
+                    const { key, newWhen } = platformMatch
+                    current.when = newWhen
+                    const objectWithPlatformKey = { [key]: current.key }
+
+                    // TODO HACK useSpecialModifiers here
+                    // eslint-disable-next-line @typescript-eslint/dot-notation
+                    if (originalKeybinding?.['win']?.startsWith('ctrl') && current.key?.startsWith('cmd')) {
+                        originalKeybinding.key = `ctrlCmd${current.key.slice('cmd'.length)}`
+                        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete, @typescript-eslint/dot-notation
+                        delete originalKeybinding['win']
+                        return prev
+                    }
+
+                    delete current.key
+
+                    Object.assign(originalKeybinding ?? current, objectWithPlatformKey)
+                }
+
+                if (!originalKeybinding) prev.push(current)
+
+                return prev
+            }, [])
+            // #endregion
+            for (const [key, keybindings] of Object.entries(categories))
+                output += `\t"${key}": [\n${keybindings
+                    .map(({ comment, universal, ...keybinding }) =>
+                        ((comment as string) + JSON.stringify(keybinding, undefined, '\t'))
+                            .split('\n')
+                            .map(line => `\t\t${line}`)
+                            .join('\n'),
+                    )
+                    .join(',\n')}\n\t],\n`
+
+            output += '}'
+
+            return new TextEncoder().encode(output)
+
+            function getPlatformFromContext(when: string | undefined) {
+                if (!when) return
+                const match = when && Object.entries(platformWhenContextMap).find(([, context]) => new RegExp(`^${context}( |$)`).exec(when))
+                if (!match) return
+                const [key, contextKey] = match
+                return {
+                    key,
+                    newWhen: when.slice(contextKey.length + 3).trim(),
+                }
             }
-
-            return new TextEncoder().encode(JSON.stringify(categories, undefined, 4 /* todo use editor tab size */))
         },
         async rename(oldUri, newUri) {
             throw new Error('Rename unsupported')
@@ -109,11 +218,80 @@ export default () => {
         watch() {
             return { dispose() {} }
         },
-        async writeFile(uri, content) {},
+        async writeFile(uri, content) {
+            const parsed: AllKeybindingsOutput | undefined = {
+                ...getCategoriesWithEmptyArrays(),
+                ...parse(new TextDecoder().decode(content), [], { allowEmptyContent: true, allowTrailingComma: true }),
+            }
+            if (!parsed) throw new Error('No contents')
+            const allBinds: any[] = []
+
+            const addWhen = (when: string | undefined, platform: keyof typeof platformWhenContextMap) => {
+                const addWhenContext = platformWhenContextMap[platform]
+                return addWhenContext + (when ? ` && ${when}` : '')
+            }
+
+            // TODO:
+            // 1. basic + (not sure: advanced) linting
+            // 2. when completions
+            for (const [index, bind] of parsed.universal.entries()) {
+                if (bind.key) {
+                    const getKeyPartWithSep = (part: string) => {
+                        let sep = ''
+                        const clean = part.replace(/^[+ ]/, s => {
+                            sep = s
+                            return ''
+                        })
+                        return [sep, clean] as const
+                    }
+
+                    const parts = bind.key.split(/(?=[+ ])/)
+                    if (parts.some(part => specialModifiers[part.replace(/^[+ ]/, '')]))
+                        for (const platform of universalPlatformKey) {
+                            const newKey = parts
+                                .map(partWithSep => {
+                                    const [sep, part] = getKeyPartWithSep(partWithSep)
+                                    return sep + ((specialModifiers[part]?.[platform] ?? part) as string)
+                                })
+                                .join('')
+                            // eslint-disable-next-line max-depth
+                            if (newKey === bind.key) continue
+                            bind[platform] = newKey
+                        }
+                }
+
+                const bindKeys = Object.keys(bind)
+                if (universalPlatformKey.every(platform => !(bindKeys as string[]).includes(platform)))
+                    throw new Error(`Keybinding with index ${index} is going to be skipped, because of no platform-dep keys / modifiers.`)
+
+                for (const platform of universalPlatformKey) {
+                    const platformKey = bind[platform]
+                    if (!platformKey) continue
+                    allBinds.push({
+                        ...pickObj(bind, 'command'),
+                        key: platformKey,
+                        when: addWhen(bind.when, platform),
+                        universal: index,
+                    })
+                }
+            }
+
+            for (const keyToReplace of universalPlatformKey)
+                for (const bind of parsed[keyToReplace])
+                    allBinds.push({
+                        ...bind,
+                        when: addWhen(bind.when, keyToReplace),
+                    })
+
+            allBinds.push(...parsed.removed, ...parsed.other)
+
+            const tabSize = vscode.workspace.getConfiguration('', null).get<number>('editor.tabSize')
+            await vscode.workspace.fs.writeFile(keybindingsFile, new TextEncoder().encode(JSON.stringify(allBinds, undefined, tabSize)))
+        },
     })
 
     registerExtensionCommand('managePlatformKeybindings', async () => {
-        await vscode.window.showTextDocument(vscode.Uri.from({ scheme: SCHEME, path: '/platform/keybindings.json' }))
+        await vscode.window.showTextDocument(vscode.Uri.from({ scheme: SCHEME, path: '/crossKeybindings.json' }))
     })
 
     vscode.window.onDidChangeActiveTextEditor(editor => {
